974d6509a33f153cf0e25d833f8ea6d75dc6cf06
ibra9001 <ibrakurbanov10gmail.com>
2023-03-04 20:21:36 +0500

    таймер

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.example.myapplication
 
-import android.content.IntentSender.OnFinished
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageSwitcher
-import android.widget.ImageView
-import android.widget.Switch
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var timer: CountDownTimer
@@ -26,13 +15,13 @@ class MainActivity : AppCompatActivity() {
 
 
         val root = findViewById<ConstraintLayout>(R.id.root)
-        val textView = findViewById<AppCompatTextView>(R.id.textView)
-        val count2 = findViewById<AppCompatButton>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val count2 = findViewById<Button>(R.id.button)
 
 
         val timer = object : CountDownTimer(10000, 1000) {
             override fun onTick(remaining: Long) {
-                textView.text = remaining.toString()
+                textView.text = (remaining / 1000).toString()
 
             }
 
@@ -41,11 +30,12 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        count2.setOnClickListener {
+            timer.start()
+        }
     }
-    override fun onStart() {
-        super.onStart()
-        timer.start()
-    }
+
 
     override fun onStop() {
         super.onStop()
