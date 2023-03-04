4586597b173c3f169bbb5e08dffcd7dbb603983d
ibra9001 <ibrakurbanov10gmail.com>
2023-03-04 18:59:56 +0500

    таймер

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.content.IntentSender.OnFinished
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -12,32 +13,44 @@ import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//     val timer = object : CountDownTimer(millislnFuture: 10000){
-//         override fun onTick (milliUntilFinished: Long) {
-//             println(milliUntilFinished / 1000)
-//         }
-//            override fun OnFinished() {
-//                println("Finished")
-//            }
-//        }
 
+        val root = findViewById<ConstraintLayout>(R.id.root)
+        val textView = findViewById<AppCompatTextView>(R.id.textView)
+        val count2 = findViewById<AppCompatButton>(R.id.button)
+
+
+        val timer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(remaining: Long) {
+                textView.text = remaining.toString()
+
+            }
+
+            override fun onFinish() {
+                textView.text = "Done!"
+            }
+
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        timer.start()
     }
 
-
-
-
-        fun countMe(view: View){
-        val countString = textView.text.toString()
-            var count : Int = Integer.parseInt(countString)
-            count++
-
-            textView.text =count.toString()
+    override fun onStop() {
+        super.onStop()
+        timer.cancel()
     }
 
 }
+
